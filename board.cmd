2d953cf99302c2520033ab2d6adbbc87d689cc40
pscrespo <pscrespo@gmail.com>
2017-09-27 12:39:32 -0300

    Initial release

---
+++ board.cmd
@@ -0,0 +1,350 @@
+# Pin name action command file
+
+# Start of element C8
+ChangePinName(C8, 2, 2)
+ChangePinName(C8, 1, 1)
+
+# Start of element C7
+ChangePinName(C7, 2, 2)
+ChangePinName(C7, 1, 1)
+
+# Start of element U5
+ChangePinName(U5, 2, OUT)
+ChangePinName(U5, 1, GND)
+ChangePinName(U5, 3, IN)
+
+# Start of element X_MOTOR
+ChangePinName(X_MOTOR, 4, 4)
+ChangePinName(X_MOTOR, 3, 3)
+ChangePinName(X_MOTOR, 2, 2)
+ChangePinName(X_MOTOR, 1, 1)
+
+# Start of element Y_MOTOR
+ChangePinName(Y_MOTOR, 4, 4)
+ChangePinName(Y_MOTOR, 3, 3)
+ChangePinName(Y_MOTOR, 2, 2)
+ChangePinName(Y_MOTOR, 1, 1)
+
+# Start of element Z_MOTOR
+ChangePinName(Z_MOTOR, 4, 4)
+ChangePinName(Z_MOTOR, 3, 3)
+ChangePinName(Z_MOTOR, 2, 2)
+ChangePinName(Z_MOTOR, 1, 1)
+
+# Start of element E_MOTOR
+ChangePinName(E_MOTOR, 4, 4)
+ChangePinName(E_MOTOR, 3, 3)
+ChangePinName(E_MOTOR, 2, 2)
+ChangePinName(E_MOTOR, 1, 1)
+
+# Start of element Q2
+ChangePinName(Q2, D, Drain)
+ChangePinName(Q2, S, Source)
+ChangePinName(Q2, G, Gate)
+
+# Start of element Q3
+ChangePinName(Q3, D, Drain)
+ChangePinName(Q3, S, Source)
+ChangePinName(Q3, G, Gate)
+
+# Start of element Q1
+ChangePinName(Q1, D, Drain)
+ChangePinName(Q1, S, Source)
+ChangePinName(Q1, G, Gate)
+
+# Start of element CONN1
+ChangePinName(CONN1, 44, 3V3)
+ChangePinName(CONN1, 43, SWDIO)
+ChangePinName(CONN1, 42, SWCLK)
+ChangePinName(CONN1, 41, GND)
+ChangePinName(CONN1, 36, PB8)
+ChangePinName(CONN1, 21, PB12)
+ChangePinName(CONN1, 28, PA11)
+ChangePinName(CONN1, 29, PA12)
+ChangePinName(CONN1, 20, GND)
+ChangePinName(CONN1, 19, GND)
+ChangePinName(CONN1, 18, 3V3)
+ChangePinName(CONN1, 17, NRST)
+ChangePinName(CONN1, 16, PB11)
+ChangePinName(CONN1, 15, PB10)
+ChangePinName(CONN1, 12, PA7)
+ChangePinName(CONN1, 8, PA3)
+ChangePinName(CONN1, 7, PA2)
+ChangePinName(CONN1, 3, PC14)
+ChangePinName(CONN1, 2, PC13)
+ChangePinName(CONN1, 1, VBAT)
+ChangePinName(CONN1, 37, PB9)
+ChangePinName(CONN1, 38, 5V)
+ChangePinName(CONN1, 39, GND)
+ChangePinName(CONN1, 40, 3V3)
+ChangePinName(CONN1, 35, PB7)
+ChangePinName(CONN1, 34, PB6)
+ChangePinName(CONN1, 33, PB5)
+ChangePinName(CONN1, 32, PB4)
+ChangePinName(CONN1, 31, PB3)
+ChangePinName(CONN1, 30, PA15)
+ChangePinName(CONN1, 22, PB13)
+ChangePinName(CONN1, 14, PB1)
+ChangePinName(CONN1, 6, PA1)
+ChangePinName(CONN1, 27, PA10)
+ChangePinName(CONN1, 26, PA9)
+ChangePinName(CONN1, 25, PA8)
+ChangePinName(CONN1, 24, PB15)
+ChangePinName(CONN1, 23, PB14)
+ChangePinName(CONN1, 13, PB0)
+ChangePinName(CONN1, 11, PA6)
+ChangePinName(CONN1, 10, PA5)
+ChangePinName(CONN1, 9, PA4)
+ChangePinName(CONN1, 5, PA0)
+ChangePinName(CONN1, 4, PC15)
+
+# Start of element R12
+ChangePinName(R12, 1, 1)
+ChangePinName(R12, 2, 2)
+
+# Start of element LED4
+ChangePinName(LED4, 2, K)
+ChangePinName(LED4, 1, A)
+
+# Start of element VIN_EXT
+ChangePinName(VIN_EXT, 1, 1)
+ChangePinName(VIN_EXT, 2, 2)
+
+# Start of element VIN_BED
+ChangePinName(VIN_BED, 1, 1)
+ChangePinName(VIN_BED, 2, 2)
+
+# Start of element Z_MIN
+ChangePinName(Z_MIN, 3, 3)
+ChangePinName(Z_MIN, 1, 1)
+ChangePinName(Z_MIN, 2, 2)
+
+# Start of element Y_MIN
+ChangePinName(Y_MIN, 3, 3)
+ChangePinName(Y_MIN, 1, 1)
+ChangePinName(Y_MIN, 2, 2)
+
+# Start of element X_MIN
+ChangePinName(X_MIN, 3, 3)
+ChangePinName(X_MIN, 1, 1)
+ChangePinName(X_MIN, 2, 2)
+
+# Start of element T_EXT
+ChangePinName(T_EXT, 1, 1)
+ChangePinName(T_EXT, 2, 2)
+
+# Start of element T_BED
+ChangePinName(T_BED, 1, 1)
+ChangePinName(T_BED, 2, 2)
+
+# Start of element C1
+ChangePinName(C1, 2, -)
+ChangePinName(C1, 1, +)
+
+# Start of element C2
+ChangePinName(C2, 2, -)
+ChangePinName(C2, 1, +)
+
+# Start of element R1
+ChangePinName(R1, 1, 1)
+ChangePinName(R1, 2, 2)
+
+# Start of element R2
+ChangePinName(R2, 1, 1)
+ChangePinName(R2, 2, 2)
+
+# Start of element HEATER_BED
+ChangePinName(HEATER_BED, 1, 1)
+ChangePinName(HEATER_BED, 2, 2)
+
+# Start of element R6
+ChangePinName(R6, 1, 1)
+ChangePinName(R6, 2, 2)
+
+# Start of element LED2
+ChangePinName(LED2, 2, K)
+ChangePinName(LED2, 1, A)
+
+# Start of element R8
+ChangePinName(R8, 1, 1)
+ChangePinName(R8, 2, 2)
+
+# Start of element R7
+ChangePinName(R7, 1, 1)
+ChangePinName(R7, 2, 2)
+
+# Start of element HEATER_EXT
+ChangePinName(HEATER_EXT, 1, 1)
+ChangePinName(HEATER_EXT, 2, 2)
+
+# Start of element R3
+ChangePinName(R3, 1, 1)
+ChangePinName(R3, 2, 2)
+
+# Start of element LED1
+ChangePinName(LED1, 2, K)
+ChangePinName(LED1, 1, A)
+
+# Start of element R5
+ChangePinName(R5, 1, 1)
+ChangePinName(R5, 2, 2)
+
+# Start of element R4
+ChangePinName(R4, 1, 1)
+ChangePinName(R4, 2, 2)
+
+# Start of element PRINT_FAN
+ChangePinName(PRINT_FAN, 1, 1)
+ChangePinName(PRINT_FAN, 2, 2)
+
+# Start of element R9
+ChangePinName(R9, 1, 1)
+ChangePinName(R9, 2, 2)
+
+# Start of element LED3
+ChangePinName(LED3, 2, K)
+ChangePinName(LED3, 1, A)
+
+# Start of element R11
+ChangePinName(R11, 1, 1)
+ChangePinName(R11, 2, 2)
+
+# Start of element R10
+ChangePinName(R10, 1, 1)
+ChangePinName(R10, 2, 2)
+
+# Start of element J3
+ChangePinName(J3, 1, 1)
+ChangePinName(J3, 2, 2)
+
+# Start of element C3
+ChangePinName(C3, 2, 2)
+ChangePinName(C3, 1, 1)
+
+# Start of element J1
+ChangePinName(J1, 1, 1)
+ChangePinName(J1, 2, 2)
+
+# Start of element J2
+ChangePinName(J2, 1, 1)
+ChangePinName(J2, 2, 2)
+
+# Start of element U1
+ChangePinName(U1, 16, VMOT)
+ChangePinName(U1, 15, GND2)
+ChangePinName(U1, 14, 2B)
+ChangePinName(U1, 13, 2A)
+ChangePinName(U1, 12, 1A)
+ChangePinName(U1, 11, 1B)
+ChangePinName(U1, 10, VDD)
+ChangePinName(U1, 9, GND)
+ChangePinName(U1, 8, DIR)
+ChangePinName(U1, 6, SLP)
+ChangePinName(U1, 5, RST)
+ChangePinName(U1, 4, MS3)
+ChangePinName(U1, 3, MS2)
+ChangePinName(U1, 2, MS1)
+ChangePinName(U1, 7, STEP)
+ChangePinName(U1, 1, EN)
+
+# Start of element J6
+ChangePinName(J6, 1, 1)
+ChangePinName(J6, 2, 2)
+
+# Start of element C4
+ChangePinName(C4, 2, 2)
+ChangePinName(C4, 1, 1)
+
+# Start of element J4
+ChangePinName(J4, 1, 1)
+ChangePinName(J4, 2, 2)
+
+# Start of element J5
+ChangePinName(J5, 1, 1)
+ChangePinName(J5, 2, 2)
+
+# Start of element U2
+ChangePinName(U2, 16, VMOT)
+ChangePinName(U2, 15, GND2)
+ChangePinName(U2, 14, 2B)
+ChangePinName(U2, 13, 2A)
+ChangePinName(U2, 12, 1A)
+ChangePinName(U2, 11, 1B)
+ChangePinName(U2, 10, VDD)
+ChangePinName(U2, 9, GND)
+ChangePinName(U2, 8, DIR)
+ChangePinName(U2, 6, SLP)
+ChangePinName(U2, 5, RST)
+ChangePinName(U2, 4, MS3)
+ChangePinName(U2, 3, MS2)
+ChangePinName(U2, 2, MS1)
+ChangePinName(U2, 7, STEP)
+ChangePinName(U2, 1, EN)
+
+# Start of element J9
+ChangePinName(J9, 1, 1)
+ChangePinName(J9, 2, 2)
+
+# Start of element C5
+ChangePinName(C5, 2, 2)
+ChangePinName(C5, 1, 1)
+
+# Start of element J7
+ChangePinName(J7, 1, 1)
+ChangePinName(J7, 2, 2)
+
+# Start of element J8
+ChangePinName(J8, 1, 1)
+ChangePinName(J8, 2, 2)
+
+# Start of element U3
+ChangePinName(U3, 16, VMOT)
+ChangePinName(U3, 15, GND2)
+ChangePinName(U3, 14, 2B)
+ChangePinName(U3, 13, 2A)
+ChangePinName(U3, 12, 1A)
+ChangePinName(U3, 11, 1B)
+ChangePinName(U3, 10, VDD)
+ChangePinName(U3, 9, GND)
+ChangePinName(U3, 8, DIR)
+ChangePinName(U3, 6, SLP)
+ChangePinName(U3, 5, RST)
+ChangePinName(U3, 4, MS3)
+ChangePinName(U3, 3, MS2)
+ChangePinName(U3, 2, MS1)
+ChangePinName(U3, 7, STEP)
+ChangePinName(U3, 1, EN)
+
+# Start of element J12
+ChangePinName(J12, 1, 1)
+ChangePinName(J12, 2, 2)
+
+# Start of element C6
+ChangePinName(C6, 2, 2)
+ChangePinName(C6, 1, 1)
+
+# Start of element J10
+ChangePinName(J10, 1, 1)
+ChangePinName(J10, 2, 2)
+
+# Start of element J11
+ChangePinName(J11, 1, 1)
+ChangePinName(J11, 2, 2)
+
+# Start of element U4
+ChangePinName(U4, 16, VMOT)
+ChangePinName(U4, 15, GND2)
+ChangePinName(U4, 14, 2B)
+ChangePinName(U4, 13, 2A)
+ChangePinName(U4, 12, 1A)
+ChangePinName(U4, 11, 1B)
+ChangePinName(U4, 10, VDD)
+ChangePinName(U4, 9, GND)
+ChangePinName(U4, 8, DIR)
+ChangePinName(U4, 6, SLP)
+ChangePinName(U4, 5, RST)
+ChangePinName(U4, 4, MS3)
+ChangePinName(U4, 3, MS2)
+ChangePinName(U4, 2, MS1)
+ChangePinName(U4, 7, STEP)
+ChangePinName(U4, 1, EN)
