c01baaa796f0063234502b0674cfe41bf29a752d
Pablo Crespo <pscrespo@gmail.com>
2018-05-18 15:28:11 -0400

    Change Z-Probe pin
    X and Y enable now share the same pin.
    Z probe has a new pin.
    Reformatted pin definition file.

--- pins_MORPHEUS.h
+++ pins_MORPHEUS.h
@@ -20,110 +20,65 @@
  *
  */
 
-#if !defined(__STM32F1__)
-    #error "Oops!  Make sure you have an STM32F1 board selected from the 'Tools -> Boards' menu."
-#endif
-
-
 /**
- * 21017 Victor Perez Marlin for stm32f1 test
- * Modified by SPON! for Morpheus Board
+ * MORPHEUS Board pin assignments
  */
 
-#ifndef BOARD_NAME
-  #define BOARD_NAME "Morpheus for BluePill Development Board"
+#ifndef __STM32F1__
+  #error "Oops! You must be compiling for STM32."
 #endif
 
+#define BOARD_NAME "Bluepill based board"
 
-#define LARGE_FLASH true
-
-#define X_STEP_PIN         PB8
-#define X_DIR_PIN          PB7
-#define X_ENABLE_PIN       PB9
+//
+// Limit Switches
+//
 #define X_MIN_PIN          PB14
-#define X_MAX_PIN          -1
-
-#define Y_STEP_PIN         PB5
-#define Y_DIR_PIN          PB4
-#define Y_ENABLE_PIN       PB6
 #define Y_MIN_PIN          PB13
-#define Y_MAX_PIN          -1
-
-
-#define Z_STEP_PIN         PA15
-#define Z_DIR_PIN          PA10
-#define Z_ENABLE_PIN       PB3
 #define Z_MIN_PIN          PB12
-#define Z_MAX_PIN          -1
 
 //
 // Z Probe (when not Z_MIN_PIN)
 //
 #ifndef Z_MIN_PROBE_PIN
-  #define Z_MIN_PROBE_PIN  PA3
+  #define Z_MIN_PROBE_PIN  PB9
 #endif
 
-#define Y2_STEP_PIN        -1
-#define Y2_DIR_PIN         -1
-#define Y2_ENABLE_PIN      -1
+//
+// Steppers
+//
+// X & Y enable are the same
+#define X_STEP_PIN         PB7
+#define X_DIR_PIN          PB6
+#define X_ENABLE_PIN       PB8
 
-#define Z2_STEP_PIN        -1
-#define Z2_DIR_PIN         -1
-#define Z2_ENABLE_PIN      -1
+#define Y_STEP_PIN         PB5
+#define Y_DIR_PIN          PB4
+#define Y_ENABLE_PIN       PB8
+
+#define Z_STEP_PIN         PA15
+#define Z_DIR_PIN          PA10
+#define Z_ENABLE_PIN       PB3
 
 #define E0_STEP_PIN        PA8
 #define E0_DIR_PIN         PB15
 #define E0_ENABLE_PIN      PA9
 
-#define E1_STEP_PIN        -1
-#define E1_DIR_PIN         -1
-#define E1_ENABLE_PIN      -1
+//
+// Temperature Sensors
+//
+#define TEMP_0_PIN         PB1   // Analog Input (HOTEND thermistor)
+#define TEMP_BED_PIN       PB0   // Analog Input (BED thermistor)
 
-#define SDPOWER            -1
-#define SDSS               -1
-#define LED_PIN            -1
- 
-#define FAN_PIN            PA1
+//
+// Heaters / Fans
+//
+#define HEATER_0_PIN       PA2   // HOTEND MOSFET
+#define HEATER_BED_PIN     PA0   // BED MOSFET
 
-#define PS_ON_PIN          -1
-  
-#define KILL_PIN           -1
+#define FAN_PIN            PA1   // FAN1 header on board - PRINT FAN
 
-#define HEATER_0_PIN       PA2   // EXTRUDER 1
-#define HEATER_1_PIN       -1
-#define HEATER_2_PIN       -1
- 
-#define TEMP_0_PIN         PB1   // ANALOG NUMBERING
-#define TEMP_1_PIN         -1   // ANALOG NUMBERING
-#define TEMP_2_PIN         -1   // ANALOG NUMBERING
-
-#define HEATER_BED_PIN     PA0    // BED
-
-#define TEMP_BED_PIN       PB0   // ANALOG NUMBERING
-
-#ifdef NUM_SERVOS
-  #define SERVO0_PIN         -1
-
-  #if NUM_SERVOS > 1
-    #define SERVO1_PIN         -1
-  #endif
-
-  #if NUM_SERVOS > 2
-    #define SERVO2_PIN         -1
-  #endif
-
-  #if NUM_SERVOS > 3
-    #define SERVO3_PIN         -1
-  #endif
-#endif
-
-#define E2_STEP_PIN        -1
-#define E2_DIR_PIN         -1
-#define E2_ENABLE_PIN      -1
-
-#define U_MIN_PIN          -1
-#define V_MIN_PIN          -1
-#define W_MIN_PIN          -1
-
-#define HEATER_BED2_PIN    -1    // BED2
-#define HEATER_BED3_PIN    -1    // BED3
+//
+// Misc.
+//
+#define LED_PIN            PC13
